ad804802f4e7707dcd0435449270adad44448bd2
Puskass <cosovictarik711@gmail.com>
2023-04-10 13:47:14 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DataFetching from "./components/DataFetching";
+
+function App() {
+  return (
+    <div>
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DataFetching.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./Loading";
+
+const DataFetching = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/films")
+      .then((res) => {
+        console.log(res.data.results);
+        setMovies(res.data.results);
+        setLoading(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {loading ? (
+          movies.map((movie) => <li key={movie.episode_id}> {movie.title} </li>)
+        ) : (
+          <Loading />
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default DataFetching;
